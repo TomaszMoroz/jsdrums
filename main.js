73db18d3558d5f07598bd0663583b7fc01f54359
TomaszMoroz <tmoroz688@gmail.com>
2017-09-28 11:14:38 +0200

    sounds added

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 /**
  * Created by Tomasz on 9/28/2017.
  */
+function playSound (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if (!audio) // handle null keys
+        return;
+    audio.currentTime = 0; // allow fast play
+    audio.play();
+    key.classList.add('playing');
+}
+
+function endSound (e) {
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!key)
+        return;
+    key.classList.remove('playing');
+}
+
+window.addEventListener('keydown', playSound)
+window.addEventListener('keyup', endSound)
